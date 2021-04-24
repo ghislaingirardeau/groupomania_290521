f4fb9b209660951b5c17baa468c7541a358a9275
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-04-24 18:33:01 +0800

    debug crash app if a results

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -13,44 +13,44 @@ exports.signup = (req, res, next) => { /* EMAIL UNIQUE ??? */
     bcrypt.hash(req.body.password, salt)
     .then(hash => {
 
-        const sql = `INSERT INTO Users (pseudo, email, password) VALUES ("${req.body.pseudo}", "${req.body.email}", "${hash}");`;
+        const sql = `INSERT INTO Users (pseudo, email, password) 
+            VALUES ("${req.body.pseudo}", "${req.body.email}", "${hash}");`;
         connection.query(sql, (error, results, fields) => {
-        if (error) {
-            res.status(400).json({message: 'Echec'})
-        }
-        console.log(results)
-        return res.status(201).json({message: 'user créé'})
+            if (error) {
+                res.status(400).json({message: 'Ce pseudo existe deja'})
+            }
+            else if(results){
+                res.status(201).json({message: 'user créé'})
+            }
         });
-                    
     })
     .catch(() => res.status(400).json({message: 'Echec'}))
 }
 
-/* CRASH DE APP A RESOUDRE TRY CATCH ?? */
-/* BUG lors d'une nouvelle requete apres erreur */
 /* CLE TOKEN A MASQUER */
 exports.login = (req, res, next) => { 
    
     const sql = `SELECT * FROM Users WHERE pseudo="${req.body.pseudo}"`;
-        connection.query(sql, (error, results, fields) => {
-            if (results.length == 0 || error) {
-                res.status(400).json({message: "Ce pseudo n'existe pas"})
-            }
-        
-            bcrypt.compare(req.body.password, results[0].password)
+    connection.query(sql, (error, results, fields) => {
+        if (results.length == 0 || error) {
+            res.status(400).json({message: "Ce pseudo n'existe pas"})
+        }
+        else if(results.length > 0){
+                bcrypt.compare(req.body.password, results[0].password)
             .then(valid => {
                 if (!valid){
                 return res.status(401).json({message: "Ce mot de passe n'est pas valide"})
                 }
                 res.status(200).json({
                 userId: results[0].id,
-                token: jwt.sign(
-                {userId: results[0].id}, `CLE TOKEN`,
-                { expiresIn: '24h'})  
+                    token: jwt.sign(
+                    {userId: results[0].id}, `CLE TOKEN`,
+                    { expiresIn: '24h'})  
                 })
             })
-            .catch(() => res.status(500).json({message: "erreur login"}))         
-        });
+            .catch(() => res.status(500).json({message: "erreur login"}))
+        }           
+    });
 }
 
 /* LOGIN
