cad0f0c9b5c66f373c531cca077b4a1842c3a69a
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-05-01 10:19:33 +0800

    changement table users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,8 +2,8 @@ const express = require('express')
 const userControle = require('../controllers/users')
 const router = express.Router()
 
-router.post('/signup', userControle.signup) /* REQUETE ATTENDU { "pseudo": "XXX", "email":"XXX" , "password": "XXX"}  */
-router.post('/login', userControle.login) /* REQUETE ATTENDU { "pseudo":"XXX", "password": "XXXX"} */
-router.delete('/remove', userControle.deleteAccount) /* REQUETE ATTENDU { "pseudo":"XXX", "email":"XXX" "password": "XXXX"} */
+router.post('/signup', userControle.signup) /* REQUETE ATTENDU { "username": "XXX", "email":"XXX" , "password": "XXX"}  */
+router.post('/login', userControle.login) /* REQUETE ATTENDU { "username":"XXX", "password": "XXXX"} */
+router.delete('/remove', userControle.deleteAccount) /* REQUETE ATTENDU { "username":"XXX", "email":"XXX" "password": "XXXX"} */
 
 module.exports = router
