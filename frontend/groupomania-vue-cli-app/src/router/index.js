fa5c61372d745d18f5bd49eec62f100b4de1c9c0
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-05-06 11:26:50 +0800

    config routes sujet et home

--- frontend/groupomania-vue-cli-app/src/router/index.js
+++ frontend/groupomania-vue-cli-app/src/router/index.js
@@ -1,19 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Compte from '../views/Home.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/compte',
+    name: 'Compte',
+    component: Compte
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/sujet',
+    name: 'sujet',
+    component: () => import('../views/sujet.vue')
   }
 ]
 
