eba3a992cfd9a9e5aab132a2f61b82163b23b0d9
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-05-06 11:41:43 +0800

    test de data sujet

--- frontend/groupomania-vue-cli-app/src/views/Home.vue
+++ frontend/groupomania-vue-cli-app/src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <header class="home">
+    
+  </header>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Compte',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
--- frontend/groupomania-vue-cli-app/src/views/sujet.vue
+++ frontend/groupomania-vue-cli-app/src/views/sujet.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <section class="sujet">
+    <h1>BIENVENUE SUR LE FORUM Groupomania</h1>
+    <article> 
+      <h2>{{title}}</h2>
+
+    </article>
+  </section>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: "Suivez l'actualité des derniers sujets commentés"
+    }
+  },
+}
+</script>
