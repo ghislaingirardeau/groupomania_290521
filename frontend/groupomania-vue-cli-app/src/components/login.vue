8dc8dcb3812c7e528e1572389586f2166c56b261
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-05-10 10:52:44 +0200

    prepare template login

---
+++ frontend/groupomania-vue-cli-app/src/components/login.vue
@@ -0,0 +1,38 @@
+<template>
+    <article id="login">
+      <h2>Login</h2>
+      <form action="home">
+      <label for="login">Username</label>
+      <input for="login" type="text" v-model="post.username">
+      <label for="login">Password</label>
+      <input for="login" type="text" v-model="post.password">
+      <input type="submit" value="Envoyer !" @click="postSubject">
+      </form>
+    </article>
+</template>
+
+<script>
+export default {
+    name: "login",
+    data () {
+        return {
+          post: {
+            username: null,
+            password: null
+          }
+        }
+    },
+    methods: {
+        postSubject() { 
+          fetch("http://localhost:3000/api/compte/login", {
+          method: 'POST',
+          headers: {"content-type": "application/json"},
+          body: JSON.stringify(this.post)
+          })
+          .then (res => res.json())
+          .catch(() => console.log({message: "connexion impossible"}))
+        }
+    },
+}
+</script>
+
--- frontend/groupomania-vue-cli-app/src/views/account.vue
+++ frontend/groupomania-vue-cli-app/src/views/account.vue
@@ -1,14 +1,19 @@
 <template>
     <div id="account">
         Ceci est la page du login
+        <login/>
     </div>
 </template>
 
 <script>
+import login from '../components/login.vue'
+
 export default {
     name: "account",
     data () {
-        
     },
+    components: {
+        login
+    }  
 }
 </script>
