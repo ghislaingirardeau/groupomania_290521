04593a34116d020c4947a52dada30c003994e8fc
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-05-12 14:58:01 +0200

    ajout controle auth pour le delete account

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const userControle = require('../controllers/users')
+const authentification = require('../Middleware/auth')
 const router = express.Router()
 
 router.post('/signup', userControle.signup) /* REQUETE ATTENDU { "username": "XXX", "email":"XXX" , "password": "XXX"}  */
 router.post('/login', userControle.login) /* REQUETE ATTENDU   { "username": "XXX", "password": "XXX"}  */
-router.delete('/remove', userControle.deleteAccount) /* REQUETE ATTENDU { "username":"XXX", "email":"XXX" "password": "XXXX"} */
+router.delete('/remove', authentification, userControle.deleteAccount) /* REQUETE ATTENDU { "username":"XXX", "email":"XXX" "password": "XXXX"} */
 
 module.exports = router
