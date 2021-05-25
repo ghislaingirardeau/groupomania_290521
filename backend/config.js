dccae54e54250b84eab05205b4bc4f9ccefe04f0
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-05-25 17:07:25 +0200

    changement du remote sql plus stable

--- backend/config.js
+++ backend/config.js
@@ -2,7 +2,7 @@
 
 /* Pour une connection en ligne à mysql exporter configWeb (par défaut) */
 let configWeb = {
-    host: 'db4free.net',
+    host: 'freedb.tech',
     port: 3306,
     user: `${process.env.USER_DB}`,
     password: `${process.env.PASSWORD_DB}`,
